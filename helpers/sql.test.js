d61673dfed75af58c622ff966b99b14dd815d860
Brianna Tran <briannatran45@gmail.com>
2023-09-20 10:59:03 -0700

    added 2 tests

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -2,12 +2,32 @@ const { BadRequestError } = require("../expressError");
 const { sqlForPartialUpdate } = require("./sql");
 
 describe("sqlForPartialUpdate", function () {
-    test("Test no data in dataToUpdate", function () {
+    test("update with no data", function () {
         let dataToUpdate = {};
         let jsToSql = { firstName: "first_name" };
 
         expect(() => {
             sqlForPartialUpdate(dataToUpdate, jsToSql);
         }).toThrow("No data");
+    });
+
+    test("updating one column", function () {
+        let dataToUpdate = { firstName: 'Alice' };
+        let jsToSql = { firstName: "first_name" };
+
+        const results = sqlForPartialUpdate(dataToUpdate, jsToSql);
+
+        expect(results.setCols).toEqual('"first_name"=$1');
+        expect(results.values).toEqual(['Alice']);
+    });
+
+    test("updating more than one column", function () {
+        let dataToUpdate = { firstName: 'Alice', age: 32 };
+        let jsToSql = { firstName: "first_name", age: "age" };
+
+        const results = sqlForPartialUpdate(dataToUpdate, jsToSql);
+
+        expect(results.setCols).toEqual('"first_name"=$1, "age"=$2');
+        expect(results.values).toEqual(['Alice', 32]);
     });
 });
