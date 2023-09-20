83a935bfd58f7fdaf7084b658b4a561146072f9e
Gabe Yoro <gabrielyoro@gmail.com>
2023-09-20 12:15:58 -0700

    init check query

---
+++ helpers/checkQuery.js
@@ -0,0 +1,6 @@
+// // generate if query
+// if (req.query.maxEmployees) {
+//     array.push(`numEmployees <= ${req.query.maxEmployees}`);
+// }
+// // array.join(", OR ");
+// const where = array.length > 0 ? `WHERE ${array}` : "";
