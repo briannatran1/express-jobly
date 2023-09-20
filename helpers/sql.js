461ab96229866fda95ac9c900473b2cadecbea83
Brianna Tran <briannatran45@gmail.com>
2023-09-20 10:58:54 -0700

    added documentation

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,21 +2,30 @@
 
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Updates specific columns in database.
+ *
+ * Accepts 2 objects:
+ * 1st obj => what values we want to update/change like {firstName: 'Alice'}
+ * 2nd obj => columns that correspond to our db (finds column in db and updates with new value)
+ *
+ * Return obj with db columns in a string and updated values in an arr like,
+ * => {setCols: '"first_name"=$1, values: ['Alice']}
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
-    const keys = Object.keys(dataToUpdate);
-    if (keys.length === 0) throw new BadRequestError("No data");
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
 
-    // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-    const cols = keys.map(
-        (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
-    );
+  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
+  );
 
-    return {
-        setCols: cols.join(", "),
-        values: Object.values(dataToUpdate),
-    };
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
 }
 
 module.exports = { sqlForPartialUpdate };
