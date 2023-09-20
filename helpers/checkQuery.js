d0962b84c1175d7b1f002f42f952756337dca629
Brianna Tran <briannatran45@gmail.com>
2023-09-20 12:32:28 -0700

    started helper fn

--- helpers/checkQuery.js
+++ helpers/checkQuery.js
@@ -1,6 +1,27 @@
-// // generate if query
-// if (req.query.maxEmployees) {
-//     array.push(`numEmployees <= ${req.query.maxEmployees}`);
-// }
-// // array.join(", OR ");
-// const where = array.length > 0 ? `WHERE ${array}` : "";
+
+
+/**
+ * checkQuery:
+ * checking query string properties, if present, and saving the values in an array
+ */
+
+function checkQuery() {
+  // key error if one missing?
+
+  const values = [];
+
+  if (req.query.maxEmployees) {
+    values.push(`numEmployees <= ${req.query.maxEmployees}`);
+  }
+
+  if (req.query.minEmployees) {
+    values.push(`numEmployees >= ${req.query.minEmployees}`);
+  }
+
+  if (req.query.nameLike) {
+    values.push(`WHERE name ILIKE %${req.query.nameLike}%`);
+  }
+
+  const where = values.length > 0 ? `WHERE ${values.join(', OR ')}` : '';
+  return where;
+}
