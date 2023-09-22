b7d88e8536cd0e588021f9c692a07facdd544c37
Brianna Tran <briannatran45@gmail.com>
2023-09-22 09:42:50 -0700

    created job routes

---
+++ routes/jobs.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/** Routes for jobs. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureLoggedIn, isAdmin } = require("../middleware/auth");
+const Job = require("../models/job");
+
+const router = new express.Router();
